faea04561173b6efba4dc4ff84a30ceac8154a3c
Khuzaima <muhammedkhuzaimasheikhqureshi@gmail.com>
2026-02-05 11:50:16 -0500

    Initial sharing of project

---
+++ TicketMachine.java
@@ -0,0 +1,127 @@
+/**
+ * TicketMachine models a naive ticket machine that issues
+ * flat-fare tickets.
+ * The price of a ticket is specified via the constructor.
+ * It is a naive machine in the sense that it trusts its users
+ * to insert enough money before trying to print a ticket.
+ * It also assumes that users enter sensible amounts.
+ *
+ * @author David J. Barnes and Michael Kölling
+ * @version 7.0
+ */
+public class TicketMachine
+{
+    // The price of a ticket from this machine.
+    private int price;
+    // The amount of money entered by a customer so far.
+    private int balance;
+    // The total amount of money collected by this machine.
+    private int total;
+    private int attempts;
+
+    /**
+     * Create a machine that issues tickets of the given price.
+     * Note that the price must be greater than zero, and there
+     * are no checks to ensure this.
+     */
+    public TicketMachine(int cost)
+    {
+        price = cost;
+        balance = 0;
+        total = 0;
+    }
+
+    public TicketMachine(){
+        price = 123;
+        balance= 0;
+        total=0;
+    }
+
+    public void empty(){
+        balance= balance- balance;
+    }
+
+    public void prompt(){
+        System.out.println("Please insert the correct amount of money");
+    }
+
+    /**
+     * Return the price of a ticket.
+     */
+    public int getPrice()
+    {
+        return price;
+    }
+
+    /**
+     * Return the amount of money already inserted for the
+     * next ticket.
+     */
+    public int getBalance()
+    {
+        return balance;
+    }
+
+    /**
+     * Receive an amount of money from a customer.
+     */
+    public void insertMoney(int amount)
+    {
+        if(amount<=0){
+            attempts++;
+            if(attempts<3) {
+                System.out.println("Pick a whole number that is greater than 0");
+            } else if(attempts==3){
+                System.out.println("Bro it ain't that hard to put a positive whole number 😭😭");
+            } else if(attempts==4) {
+                System.out.println("This ain't it lil bro 😭✌🥀🥀🥀🥀🔥💤");
+            } else if(attempts==5){
+                System.out.println("Just pack it up gng the big leagues ain't calling yo🥀✌✌🔥🔥");
+            } else if(attempts>=6){
+                System.out.println("damn... Just take your time I guess");
+            }
+        } else{
+            balance = balance + amount;
+            attempts= 0;
+        } 
+    }
+
+    public void showPrice(){
+        System.out.println("The price of a ticket is " + price + " cents");
+    }
+
+    /**
+     * Print a ticket.
+     * Update the total collected and
+     * reduce the balance to zero.
+     */
+    public void printTicket()
+    {
+        if(balance>= price){
+            System.out.println("##################");
+            System.out.println("# The BlueJ Line");
+            System.out.println("# Ticket");
+            System.out.println("# " + price + " cents.");
+            System.out.println("##################");
+            System.out.println();
+
+            total = total + balance;
+            balance = 0;
+        } else{
+            prompt();
+        }
+
+        // Simulate the printing of a ticket.
+        /*System.out.println("##################");
+        System.out.println("# The BlueJ Line");
+        System.out.println("# Ticket");
+        System.out.println("# " + price + " cents.");
+        System.out.println("##################");
+        System.out.println();*/
+
+        // Update the total collected with the balance.
+        /*total = total + balance;
+        // Clear the balance.
+        balance = 0;*/
+    }
+}
